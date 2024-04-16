9f8b1a4f1c26a6c8c029e26e5fe578cdd5656884
E_o_dede <140509312+DzinnnXn@users.noreply.github.com>
2024-04-16 10:29:24 -0300

    Update Cadastro.tsx

---
+++ app/TelasIniciais/Cadastro.tsx
@@ -0,0 +1,37 @@
+import { StyleSheet, View, Text } from "react-native";
+import Header from "@comp/header";
+import Input from "@comp/input";
+import LinkBtn from "@comp/button";
+import { useColor } from "../../temas/Temas";
+
+export default function Cadastro(){
+
+    const styles = StyleSheet.create({
+        btnAdd: {
+            position: "absolute",
+            bottom: 30,
+            right: 30
+        }
+    })
+    return(
+        <>
+        <View>
+        <Header cor="#FF0000" texto="Cadastre-se"/>
+        </View>
+
+        <View style={styles.container}>
+        <Input nome="Nome:" placeholder="Insira o seu nome:"/>
+        <Input nome="Sobrenome:" placeholder="Insira o seu sobrenome:"/>
+        <Input nome="Email:" placeholder="Insira o seu telefone:"/>
+        <Input nome="Senha:" placeholder="Insira a sua senha:"/>
+        <Input nome="Senha:" placeholder="Insira a sua senha:" secureTextEntry={true} />
+        <LinkBtn title="Cadastre-se" href="TabNav" />
+        </View>
+
+
+
+        
+        </>
+    )
+}
+
