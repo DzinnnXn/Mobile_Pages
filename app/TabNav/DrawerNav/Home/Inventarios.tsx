fde0d080fe5550099ba07131c0647a9531a8a796
E_o_dede <140509312+DzinnnXn@users.noreply.github.com>
2024-04-18 12:58:11 -0300

    Update Inventarios.tsx

--- app/TabNav/DrawerNav/Home/Inventarios.tsx
+++ app/TabNav/DrawerNav/Home/Inventarios.tsx
@@ -1,10 +1,49 @@
-import { Tabs } from "expo-router";
+import { StyleSheet, View, Text, ScrollView, TouchableOpacity } from "react-native";
+import Nav from "@comp/nav";
+import Produto from "@comp/produto";
+import Footer from "@comp/footer";
+import { Feather } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import Btn from "@comp/btn";
+import { AntDesign } from '@expo/vector-icons';
 
-export default function Layout(){
+
+export default function Inventarios(){
     return(
-        <Tabs>
-            <Tabs.Screen name="Scanner"/>
-            <Tabs.Screen name="DrawerNav"/>
-        </Tabs>
+        <>
+        <View style={styles.container}>
+
+
+        <ScrollView >
+        <Produto numero="956389" nome="CADEIRA SENAI"/>
+        <Produto numero="956389" nome="CADEIRA SENAI"/>
+        <Produto numero="956389" nome="CADEIRA SENAI"/>
+        <Produto numero="956389" nome="CADEIRA SENAI"/>
+        <Produto numero="956389" nome="CADEIRA SENAI"/>
+        <Produto numero="956389" nome="CADEIRA SENAI"/>
+        <Produto numero="956389" nome="CADEIRA SENAI"/>
+        <Produto numero="956389" nome="CADEIRA SENAI"/>
+        <Produto numero="956389" nome="CADEIRA SENAI"/>
+        <Produto numero="956389" nome="CADEIRA SENAI"/>
+        <Produto numero="956389" nome="CADEIRA SENAI"/>
+        <Produto numero="956389" nome="CADEIRA SENAI"/>
+        <Produto numero="956389" nome="CADEIRA SENAI"/>
+        <Produto numero="956389" nome="CADEIRA SENAI"/>
+        <Produto numero="956389" nome="CADEIRA SENAI"/>
+        <Produto numero="956389" nome="CADEIRA SENAI"/>
+        </ScrollView>
+        
+        <Btn icone={<AntDesign name="pluscircle" size={70} color="#ff0000" />} />
+        </View>
+        
+        </>
     )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        position:'relative',
+      },
+    });
