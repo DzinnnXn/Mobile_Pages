4ef6a990c8e26f64df30b4576c284571b52b9c02
E_o_dede <140509312+DzinnnXn@users.noreply.github.com>
2024-04-24 18:21:43 -0300

    Update Scanner.tsx

--- app/TabNav/Scanner.tsx
+++ app/TabNav/Scanner.tsx
@@ -1,40 +1,61 @@
-import { AntDesign } from '@expo/vector-icons';
-import { Ionicons } from '@expo/vector-icons';
-import { StyleSheet, View, Image, ScrollView} from 'react-native';
-import Nav from '@comp/nav';
-import Footer from '@comp/footer';
-export default function Scanner(){
+import React, { useState } from "react";
+import { View, StyleSheet, TouchableOpacity} from "react-native";
+import {MaterialIcons} from '@expo/vector-icons';
+import { Camera, CameraType } from "expo-camera";
+
+
+const Scanner = () => {
+    const [type, setType] = useState(CameraType.back);
+    const [permission, requestPermission] = Camera.useCameraPermissions();
+    const [iconOpacity, setIconOpacity] = useState(1); 
+
+    if (!permission)
+        return null;
+
+    if (!permission.granted)
+        return null;
+
+
     return(
-        <>
-
-
-            <ScrollView>
-
-            <View style={styles.container}>
-            <Image
-        
-            source={require('@assets/Vector.png')}
-            />
-            </View>
-            </ScrollView>
-        
-
-        </>
-    )
+        <View style={styles.container}>
+            <Camera style={styles.camera} type={type}>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.button}>
+                        <MaterialIcons name="qr-code-scanner" size={300} color="#5D5C5C" style={{ opacity: iconOpacity }} />
+                    </TouchableOpacity>
+                </View>
+            </Camera>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        width: '100%',
-        height: 525,
-        backgroundColor: '#FBFBFB',
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignContent: 'center',
+      },
+    camera: {
+        flex: 1,
+        justifyContent: 'center',
+        alignContent: 'center',
         alignItems: 'center',
-        justifyContent:'center'
+        paddingTop: 240,
     },
-    cont2:{
-        height: 200,
-    }
+    buttonContainer: {
+        flex: 1,
+        backgroundColor: 'transparent',
+    },
+    button: {
+        flex: 1,
+        alignSelf: 'flex-end',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+});
 
-}
-
-)
+export default Scanner;
