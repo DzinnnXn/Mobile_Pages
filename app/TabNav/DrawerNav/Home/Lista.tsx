6ce17a4394406c1d731154c771ef6383a8afc92e
E_o_dede <140509312+DzinnnXn@users.noreply.github.com>
2024-05-07 21:13:22 -0300

    Alteração da rota

--- app/TabNav/DrawerNav/Home/Lista.tsx
+++ app/TabNav/DrawerNav/Home/Lista.tsx
@@ -1,59 +1,21 @@
-import React from "react";
-import { View, StyleSheet, ScrollView } from "react-native";
-import { AntDesign } from '@expo/vector-icons';
-import { Link } from "expo-router";
-import Itens from "@comp/Itens";
-import { useColor } from "../../../../temas/temas";
+import { DrawerToggleButton } from "@react-navigation/drawer";
+import { Stack } from "expo-router";
+import { useColor } from "../../../../temas/Temas";
 
-const Patrimonio = () => {
+export default function layout() {
     const cores = useColor();
-
-    return(
-        <View style={[styles.container, {backgroundColor:cores.bgPrimary}]}>
-            <ScrollView>
-                <Itens id={956389} item={'CADEIRA SENAI'} />
-                <Itens id={956389} item={'CADEIRA SENAI'} />
-                <Itens id={956389} item={'CADEIRA SENAI'} />
-                <Itens id={956389} item={'CADEIRA SENAI'} />
-                <Itens id={956389} item={'CADEIRA SENAI'} />
-                <Itens id={956389} item={'CADEIRA SENAI'} />
-                <Itens id={956389} item={'CADEIRA SENAI'} />
-                <Itens id={956389} item={'CADEIRA SENAI'} />
-                <Itens id={956389} item={'CADEIRA SENAI'} />
-                <Itens id={956389} item={'CADEIRA SENAI'} />
-                <Itens id={956389} item={'CADEIRA SENAI'} />
-                <Itens id={956389} item={'CADEIRA SENAI'} />
-                <Itens id={956389} item={'CADEIRA SENAI'} />
-                <Itens id={956389} item={'CADEIRA SENAI'} />
-            </ScrollView>
-
-            <View style={styles.addButtonContainer}>
-                <Link href="/Additens" asChild>
-                    <AntDesign name="pluscircle" size={50} color="red"  />
-                </Link>
-            </View>
-
-
-
-        </View>
-    );
+    return (
+        <Stack>
+            <Stack.Screen
+                name="Lista"
+                options={{
+                    headerLeft: () => <DrawerToggleButton />,
+                    headerSearchBarOptions: { placeholder: "Pesquisar" },
+                    headerStyle: { backgroundColor: cores.bgPatrimonio },
+                    headerTitleAlign: 'center',
+                    headerTintColor: cores.textColorPrimary,
+                }}
+            />
+        </Stack>
+    )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-    },
-    addButtonContainer: {
-        position: "absolute",
-        bottom: 20,
-        right: 0,
-        margin: 20,
-    },
-    text: {
-        color: "#0000",
-        fontSize: 16,
-    }, 
-});
-    
-export default Patrimonio;
